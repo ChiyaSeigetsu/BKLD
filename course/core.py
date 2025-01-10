494be9d58631834536b63454fcb7e33c295417f1
Chi'ya Seigetsu <189008330+ChiyaSeigetsu@users.noreply.github.com>
2025-01-10 21:38:13 +0700

    Update
    update

--- course/core.py
+++ course/core.py
@@ -1,17 +1,46 @@
-import os, requests, sys
+import os, requests, sys, json
 from datetime import datetime as dt
 
 
-def check_website(url):
-    try:
-        response = requests.get(url, timeout=3)
-        if response.status_code == 200:
-            print("Connected.")
-        else:
-            print(f"Status code: {response.status_code}, Can not connect.")
-            sys.stdout.flush()
-            sys.exit("The programme is exiting...")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}\nCan not connect.")
-        sys.stdout.flush()
-        sys.exit("The programme is exiting...")
+original_filename = "cautrucmonhoc.json"
+new_filename = "cautrucmonhoc_new.json"
+
+
+def export_file():
+    if os.path.exists(original_filename):
+        hcmut_courses = "hcmut_courses"
+        if not os.path.exists(hcmut_courses):
+            os.makedirs(hcmut_courses)
+        with open(original_filename, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        for course in data:
+            msmh = course["msmh"]
+            file_name = f"{msmh}.json"
+            file_path = os.path.join(hcmut_courses, file_name)
+            with open(file_path, "w", encoding="utf-8") as output_file:
+                json.dump(course, output_file, ensure_ascii=False, indent=4)
+        print("Files have been created.")
+    else:
+        print(f"Cannot found cautrucmonhoc.json")
+        print(f"The programme is exiting...")
+        sys.exit(0)
+
+
+def overwrite_file():
+    if os.path.exists(original_filename) and os.path.exists(new_filename):
+        os.remove(original_filename)
+        os.rename(new_filename, original_filename)
+        print(f"'{original_filename}' had been created.")
+    elif os.path.exists(new_filename):
+        os.rename(new_filename, original_filename)
+        print(f"'{original_filename}' had been created.")
+    else:
+        print("Please check if there is at least file name cautrucmonhoc_new.json.\nOr try run programme again.")
+        print("The programme is exiting...")
+        sys.exit(0)
+#def download_file()
+#def compare_modified_date()
+#def file_modified_date()
+#def website_modified_date()
+#def is_file_exist()
+#def is_website_alive()
--- course/test_function.py
+++ course/test_function.py
@@ -1,8 +1,4 @@
 from core import *
 
 
-aao_url = "http://aao.hcmut.edu.vn/catalog/view/theme/default/ctdt_upload/cautrucmonhoc.json"
-url = "http://127.0.0.1"
-
-
-check_website(url)
+overwrite_file()
